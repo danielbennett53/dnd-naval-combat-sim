3fb2d9019afb57cdd7e591118e20bfb601d902ce
Daniel Bennett <daniel.bennett53@gmail.com>
2020-06-14 14:30:32 -0700

    basic interface works, kind of slow, though

--- src/Ship.py
+++ src/Ship.py
@@ -16,19 +16,19 @@ class Ship:
     step = 0.05
 
     def __init__(self, pos, direction):
-        self.position = pos
+        self.position = np.array([[pos[0]], [pos[1]]])
         self.velocity = np.array([[0], [0]])
-        self.direction = direction
+        self.direction = np.array([[direction[0]], [direction[1]]])
         self.turn_rate = 0
         self.accel = 0
-        self.start_time = 0
+        self.start_time = datetime.datetime.now()
         self.nav_DC = 0
         self.path = []
 
     def set_motion(self, thrust, turn_rate):
         if abs(turn_rate) > 1.0 or abs(thrust) > 1.0:
             return
-        
+
         self.turn_rate = turn_rate * self.max_turn_rate / 6
         self.accel = thrust * self.max_accel / 6
 
@@ -39,11 +39,13 @@ class Ship:
             new_vel = rotate(new_vel, self.turn_rate * Ship.step)
             if np.linalg.norm(new_vel) > self.max_vel:
                 new_vel = new_vel * self.max_vel / np.linalg.norm(new_vel)
+            if np.dot(new_vel.getA1(), self.direction) < 0:
+                new_vel = np.array([[0], [0]])
             new_dir = rotate(self.path[-1][2], self.turn_rate * Ship.step)
             self.path.append((new_pos, new_vel, new_dir))
-            
 
-    def start_movement(self, roll):        
+
+    def start_movement(self, roll):
         self.start_time = datetime.datetime.now()
         # Add random noise if roll is less than DC
         if roll < self.nav_DC:
@@ -80,14 +82,20 @@ class Ship:
             self.direction = self.path[-1][2]
             self.path = []
             return
-        
+
         idx = round(dt * (len(self.path) - 1) / 6)
         self.position = self.path[idx][0]
         self.velocity = self.path[idx][1]
         self.direction = self.path[idx][2]
 
+    def finished(self):
+        dt = (datetime.datetime.now() - self.start_time).total_seconds()
+        if dt > 6:
+            return True
+        else:
+            return False
 
-    def get_path(self):
+    def get_ship(self):
         pts = [
             [[-0.4], [-0.5]],
             [[-0.45], [-0.15]],
@@ -115,30 +123,39 @@ class Ship:
                 "C {},{} {},{} {},{} ".format(pts[6][0], pts[6][1], pts[7][0], pts[7][1], pts[8][0], pts[8][1]) + \
                 "S {},{} {},{} ".format(pts[9][0], pts[9][1], pts[10][0], pts[10][1]) + \
                 "Z {},{}".format(pts[11][0], pts[11][1])
-        return svg_str   
+        return svg_str
+
+    def get_path(self):
+        if not self.path:
+            return ""
+        svg_str = "M {},{} ".format(self.path[0][0].item(0), self.path[0][0].item(1))
+
+        for pt in self.path:
+            svg_str += "L {},{} ".format(pt[0].item(0), pt[0].item(1))
+        return svg_str
 
 
 class Sprinter(Ship):
-    def __init__(self, pos=np.array([[0], [0]]), direction=np.array([[1.0], [0.0]])):
+    def __init__(self, pos=[0,0], direction=[1,0]):
         self.max_vel = 80
         self.max_turn_rate = 60
         self.max_accel = 30
         self.firing_arc = [(-180, 180)]
         self.firing_range = 160
         self.width = 10
-        self.length = 40
+        self.length = 30
         self.nav_DC = 12
         Ship.__init__(self, pos, direction)
 
 
 class Galleon(Ship):
-    def __init__(self, pos=np.array([[0], [0]]),  direction=np.array([[1.0], [0.0]])):
+    def __init__(self, pos=[0,0],  direction=[1,0]):
         self.max_vel = 50
         self.max_turn_rate = 30
         self.max_accel = 15
         self.firing_arc = [(-140, -40), (40, 140)]
         self.firing_range = [300, 300]
-        self.width = 30
-        self.length = 120
+        self.width = 20
+        self.length = 70
         self.nav_DC = 15
         Ship.__init__(self, pos, direction)
--- src/main.py
+++ src/main.py
@@ -2,138 +2,250 @@ import math
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.exceptions import PreventUpdate
 import plotly.graph_objects as go
 import numpy as np
 import random
 import datetime
-from Ship import Sprinter
+from Ship import Sprinter, Galleon
 
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State, MATCH, ALL
 
 random.seed()
 app = dash.Dash(__name__)
 
 starttime = datetime.datetime.now()
 
-s = Sprinter()
-    
+ships = {
+    "A": (Sprinter([0,0]), "Green"),
+    "B": (Sprinter([20, 40]), "Blue"),
+    "C": (Sprinter([-20, 75]), "Cyan"),
+    "D": (Galleon([-50, -80]), "Blue"),
+}
+
+inputs = dict()
+input_state = dict()
 
 fig = go.Figure(
     layout=go.Layout(
         yaxis=dict(
-            scaleanchor="x",
-            scaleratio=1,
-            range=[-50, 50]
-        ),
-        shapes=[
-            dict(
-                type="path",
-                path=s.get_path(),
-                fillcolor="LightGreen",
-                line_color="DarkGreen",
-            )
-        ]
-    ),
-)
-# fig.update_yaxes(range=[-10, 10])
-# fig.update_layout(
-#     yaxis = dict(
-#         scaleanchor = "x",
-#         scaleratio = 1,
-#     ),
-#     shapes=[
-#         dict(
-#             type="path",
-#             path=get_ship_svg(135, np.array([[12.4], [-6]]), 5, 2),
-#             fillcolor="LightGreen",
-#             line_color="DarkGreen",
-#         )
-#     ]
-# )
-
-app.layout = html.Div(children=[
-    dcc.Graph(
-        id='Test',
-        figure=fig,
-        animate=False,
-        animation_options = dict(
-            frame={"redraw": True},
-        )
-    ),
-    dcc.Interval(
-        id='interval',
-        interval=100,
-        n_intervals=0,
-    )
-])
-
-@app.callback(Output('Test', 'figure'),
-             [Input('interval', 'n_intervals')])
-def update_plot(n):
-    if n == 1:
-        s.set_motion(0.9, 0.5)
-    elif n == 20:
-        s.start_movement(20)
-    elif n > 20:
-        s.update()
-    
-    f = go.Figure(
-        layout=go.Layout(
-            yaxis=dict(
                 scaleanchor="x",
                 scaleratio=1,
-                range=[-50, 50]
+                range=[-300, 300],
             ),
-            shapes=[
-                dict(
-                    type="path",
-                    path=s.get_path(),
-                    fillcolor="LightGreen",
-                    line_color="DarkGreen",
-                )
-            ]
-        ),
+    ),
+)
+
+for key, value in ships.items():
+    fig.add_shape(
+        name=key + "-ship",
+        type="path",
+        path=value[0].get_ship(),
+        line_color="Dark" + value[1],
+        fillcolor="Light" + value[1],
     )
-    return f
 
-app.run_server()
-# def rot_vector(v, theta):
-#     rot_mat = np.matrix([[math.cos(theta), math.sin(theta)],
-#                          [-math.sin(theta), math.cos(theta)]])
-#     return rot_mat * v
+    fig.add_shape(
+        name=key + "-path",
+        type="path",
+        path=value[0].get_path(),
+        line_color="Dark" + value[1],
+    )
 
-# def transform_path(path, theta, d):
-#     rot_mat = np.matrix([[math.cos(theta), math.sin(theta)],
-#                          [-math.sin(theta), math.cos(theta)]])
-#     verts = [(rot_mat * v.reshape(2,1)).getA1() for v in path.vertices] 
-#     codes = path.codes
-#     return mpath.Path(verts, codes)
+    inputs[key] = {'thrust': 0, 'steer': 0, 'roll':0}
+    input_state[key] = {'thrust': False, 'steer': False, 'roll': False}
 
 
+def create_ship_controls(name, color):
+    return html.Div(
+        className="row",
+        style={
+            "border": "solid darkgrey",
+            "background-color": color,
+            "max-height": "400px",
+        },
+        children=[
+            html.H5(name, style={"text-align": "center"}),
+            dcc.Slider(
+                className="slider-pad",
+                id={"type": "input", "ship": name, "cntrl": "thrust", "last": 0},
+                min=-1.0,
+                max=1.0,
+                step=0.05,
+                value=0,
+                included=False,
+                # updatemode='drag',
+                marks={
+                    0: {'label': 'Steady as she goes!'},
+                    -1: {'label': 'Full stop!'},
+                    1: {'label': 'Full steam ahead!'}
+            }),
+            dcc.Slider(
+                className="slider-pad",
+                id={"type": "input", "ship": name, "cntrl": "steer", "last": 0},
+                min=-1.0,
+                max=1.0,
+                step=0.05,
+                value=0,
+                included=False,
+                # updatemode='drag',
+                marks={
+                    -1: {'label': 'Hard to port!'},
+                    0: {'label': 'Dead ahead!'},
+                    1: {'label': 'Hard to starboard!'}
+            }),
+            html.Div(
+                className="row mgn",
+                children=[
+                    html.Label(
+                        "Navigation Roll:",
+                        className="six columns",
+                        style={"text-align": "right",
+                               "padding-top": "7px",
+                               "padding-right": "10px"}
+                    ),
+                    dcc.Input(
+                        className="three columns",
+                        id={"type": "input", "ship": name, "cntrl": "roll", "last": 0},
+                        type="number",
+                    ),
+                ]
+            ),
+            html.Div(
+                className="row",
+                style={"margin-top": "10px"},
+                children=[
+                    html.Label(
+                        "Current AC",
+                        className="six columns",
+                        style={"text-align": "center"}
+                    ),
+                    html.Label(
+                        "Current Speed",
+                        className="six columns",
+                        style={"text-align": "center"}
+                    ),
+                ]
+            ),
+            html.Div(
+                className="row",
+                style={"margin-bottom": "10px"},
+                children=[
+                    html.Label(
+                        "0",
+                        id={"ship": name, "type": "ac"},
+                        className="six columns",
+                        style={"text-align": "center"}
+                    ),
+                    html.Label(
+                        "0",
+                        id={"ship": name, "type": "speed"},
+                        className="six columns",
+                        style={"text-align": "center"}
+                    ),
+                ]
+            ),
+        ]
+    )
+
+app.layout = html.Div(
+    className="row",
+    children=[
+        html.Div(
+            className="three columns",
+            style={"max-height": "100vh", "overflow-y": "auto"},
+            children=[create_ship_controls(k, 'Light' + v[1]) for k, v in ships.items()]
+        ),
+        html.Div(
+        className="nine columns",
+        style={
+            "height": "95vh",
+        },
+        children=[
+            dcc.Graph(
+                style={
+                    "width": "100%",
+                    "height": "100%",
+                },
+                id='plot',
+                figure=fig,
+                animate=True,
+                animation_options = dict(
+                    frame={"redraw": True},
+                )
+            ),
+            html.Button(
+                "Start Round",
+                id="start",
+                n_clicks=0,),
+        ]),
+        dcc.Interval(
+            id='long-interval',
+            interval=1000,
+            n_intervals=0,
+        ),
+    ]
+)
 
 
+motion_in_progress = False
 
-# # s1 = Galleon()
-# s2 = Sprinter(np.array([[50.0], [75.0]]))
-# s2.start_movement(1.0, 1.0, 10)
+@app.callback([Output('plot', 'figure'),
+               Output({'type': 'input', 'ship': ALL, 'cntrl': ALL, 'last': ALL}, 'value'),
+               Output({'type': 'input', 'ship': ALL, 'cntrl': ALL, 'last': ALL}, 'id')],
+             [Input('long-interval', 'n_intervals'),
+              Input('start', 'n_clicks')],
+              [State({'type': 'input', 'ship': ALL, 'cntrl': ALL, 'last': ALL}, 'value'),
+               State({'type': 'input', 'ship': ALL, 'cntrl': ALL, 'last': ALL}, 'id')])
+def update_plot(n_intervals, n_clicks, controls, ids):
+    global fig, motion_in_progress, inputs
+    # Collect changed controls in single string for easier parsing
+    changed_ids = '\n'.join([p['prop_id'] for p in dash.callback_context.triggered])
 
-# def init():
-#     ax.set_xlim(-50, 50)
-#     ax.set_ylim(-50, 50)
-#     patch = ax.add_patch(s2.patch)
-#     return []
+    # Apply any changed inputs
+    for idx, (c, i) in enumerate(zip(controls, ids)):
+        if i['last'] != c:
+            inputs[i['ship']][i['cntrl']] = c
+        ids[idx]['last'] = c
 
-# def update(frame):
-#     path = transform_path(s2.patch.get_path(), frame * 0.2, 0)
-#     patch = mpatches.PathPatch(path, facecolor='g', alpha=0.75)
-#     return ax.add_patch(patch),
+    # If motion stops, clear inputs
+    if motion_in_progress and all([v[0].finished() for v in ships.values()]):
+        motion_in_progress = False
+        for key in inputs.keys():
+            inputs[key] = {'thrust': 0, 'steer': 0, 'roll': 0}
 
-# ani = FuncAnimation(fig, update, init_func=init, frames=20, interval=20, blit=True)
-# # for _ in range(20):
-# #     s2.step_movement()
+    # Apply inputs to sliders
+    out_vals = []
+    for i in ids:
+        out_vals.append(inputs[i['ship']][i['cntrl']])
+    out_ids = ids
 
-# # ax.add_patch(s2.patch)
-# # # # s1.draw(ax, 'b')
-# # # s2.draw(ax, 'g')
-# ax.axis('equal')
-# plt.show()
+    if 'start' in changed_ids:
+        for v in ships.values():
+            v[0].start_movement(25)
+            motion_in_progress = True
+
+    # Don't output figure unless triggered by interval
+    if 'long-interval' not in changed_ids:
+        raise PreventUpdate
+
+    if not all([v[0].finished() for v in ships.values()]):
+        for k, v in ships.items():
+            v[0].update()
+            fig.update_shapes(selector={'name': k + '-ship'}, path=v[0].get_ship())
+        return fig, out_vals, out_ids
+    else:
+        # Clear inputs if motion just finished
+        if motion_in_progress:
+            motion_in_progress = False
+            for ship, i in inputs:
+                for cntrl, _ in i:
+                    inputs[ship][cntrl] = 0
+
+        for k, v in ships.items():
+            v[0].set_motion(inputs[k]['thrust'], inputs[k]['steer'])
+            fig.update_shapes(selector={"name": k + '-path'}, path=v[0].get_path())
+
+    return fig, out_vals, out_ids
+
+app.run_server(debug=True, port=8050, host='0.0.0.0')
